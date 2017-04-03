ab5973b0656f561724df5277b359ca2b8f1f0a4a
wangluyuan <wangluyuan@yeecall.com>
2017-04-03 11:45:37 +0800

    修改底部footer

--- src/common/components/app/footer/index.js
+++ src/common/components/app/footer/index.js
@@ -2,22 +2,31 @@ require('./style.less')
 
 const React = require('react')
 
+const qrCode = require('images/qrcode.png')
+
 class Footer extends React.Component {
 
     render() {
         return (
             <footer className="footer">
                 <div className="center">
-                    <div className="footer-row">
-                        <span>哈尔滨忠恩饲用天然植物有限公司</span>
+                    <div className="left">
+                        <div className="footer-row">
+                            <span>哈尔滨忠恩饲用天然植物有限公司</span>
+                        </div>
+                        <div className="footer-row">
+                            <i className="icon-map-marker"></i>哈尔滨市香坊区木材街59-14号
+                        </div>
+                        <div className="footer-row">
+                            <a href="mailto:zhongensiyong@163.com">
+                                <i className="icon-envelope-alt"></i>zhongensiyong@163.com
+                            </a>
+                        </div>
                     </div>
-                    <div className="footer-row">
-                        <i className="icon-map-marker"></i>哈尔滨市香坊区木材街59-14号
-                    </div>
-                    <div className="footer-row">
-                        <a href="mailto:zhongensiyong@163.com">
-                            <i className="icon-envelope-alt"></i>zhongensiyong@163.com
-                        </a>
+                    <div className="right">
+                        <div className="qr-code">
+                            <img src={qrCode} alt="二维码"/>
+                        </div>
                     </div>
                 </div>
             </footer>
