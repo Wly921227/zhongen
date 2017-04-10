2c9cd7c40bd2687500d0a7ed71b983b70af01b88
wangluyuan <wangluyuan@yeecall.com>
2017-04-10 19:18:21 +0800

    添加电话号 删除二维码

--- src/common/components/app/footer/index.js
+++ src/common/components/app/footer/index.js
@@ -10,23 +10,19 @@ class Footer extends React.Component {
         return (
             <footer className="footer">
                 <div className="center">
-                    <div className="left">
-                        <div className="footer-row">
-                            <span>哈尔滨忠恩饲用天然植物有限公司</span>
-                        </div>
-                        <div className="footer-row">
-                            <i className="icon-map-marker"></i>哈尔滨市香坊区木材街59-14号
-                        </div>
-                        <div className="footer-row">
-                            <a href="mailto:zhongensiyong@163.com">
-                                <i className="icon-envelope-alt"></i>zhongensiyong@163.com
-                            </a>
-                        </div>
+                    <div className="footer-row">
+                        <span>哈尔滨忠恩饲用天然植物有限公司</span>
                     </div>
-                    <div className="right">
-                        <div className="qr-code">
-                            <img src={qrCode} alt="二维码"/>
-                        </div>
+                    <div className="footer-row">
+                        <i className="icon-map-marker"></i>哈尔滨市香坊区木材街59-14号
+                    </div>
+                    <div className="footer-row">
+                        <i className="icon-phone"></i>045155191206
+                    </div>
+                    <div className="footer-row">
+                        <a href="mailto:zhongensiyong@163.com">
+                            <i className="icon-envelope-alt"></i>zhongensiyong@163.com
+                        </a>
                     </div>
                 </div>
             </footer>
